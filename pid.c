7ed8c148e745948d02aaf6635ae37e93014f4f4c
Javier Loureiro <derethor@gmail.com>
2014-05-03 21:14:17 +0000

    clean pwm fan names

--- pid.c
+++ pid.c
@@ -4,6 +4,10 @@
 # include <stdint.h>
 # include <avr/io.h>
 
+# define PINS_WRITE_PWMFAN			PORTD
+# define PINS_DDR_PWMFAN			DDRD
+# define PINS_OFFSET_PWMFAN			3
+
 volatile static uint8_t pwm_count ;
 volatile static uint8_t pwm_heat ;
 
@@ -12,8 +16,8 @@ void pid_init   (void )
 	pwm_count = 0 ;
 	pwm_heat = 0 ;
 
-	SetBit ( DDRD , PD3 ) ;
-	ClearBit ( PORTD , PD3 ) ;
+	SetBit   ( PINS_DDR_PWMFAN   , PINS_OFFSET_PWMFAN ) ;
+	ClearBit ( PINS_WRITE_PWMFAN , PINS_OFFSET_PWMFAN ) ;
 }
 
 void pid_setheat (const uint8_t heat )
@@ -25,15 +29,14 @@ void pid_update (void )
 {
 	if ( pwm_count == 0 )
 	{
-		if (pwm_heat>0)		SetBit ( PORTD , PD3 ) ;
+		if (pwm_heat>0)	SetBit ( PINS_WRITE_PWMFAN , PINS_OFFSET_PWMFAN ) ;
 	}
 
 	if (pwm_heat<pwm_count)
 	{
-		ClearBit ( PORTD , PD3 ) ;
+		ClearBit ( PINS_WRITE_PWMFAN , PINS_OFFSET_PWMFAN ) ;
 	}
 
 	++ pwm_count ;
 	pwm_count &= 0x7f ;
-
 }

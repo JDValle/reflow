38aa5e5eb4c8e63c801fe55f2db35c823d4996a5
Javier Loureiro <derethor@gmail.com>
2014-05-16 00:27:10 +0000

    for some reason, eeprom read will crash

--- settings.c
+++ settings.c
@@ -5,7 +5,7 @@ typedef struct
 	uint8_t		version ;
 } settings_t ;
 
-settings_t eeprom_settings EEMEM = ( settings_t )
+const settings_t eeprom_settings EEMEM = ( settings_t )
     {
       .version = 0 ,
     } ;
@@ -14,20 +14,9 @@ static settings_t settings ;
 
 void settings_load (void )
 {
-	eeprom_read_block( &settings , &eeprom_settings , sizeof(settings_t) ) ;
+//	eeprom_read_block( &settings , &eeprom_settings , sizeof(settings_t) ) ;
 }
-
-/*
-int main(void) __attribute__((noreturn));
-int main (void )
-{
-
-  cli ();
-  uart_init ( UART_INIT_INTERRUPT_ENABLED );
-  sei ();
-  
-  const uint8_t b = eeprom_read_byte ( &byte0 ) ;
-*/
 
 // FAN MIN / FAN MAX
 // Kp Ki Kd
+// buzzer on/off
